960fe8b5bd2b44924403475ec2a22cc7d764747a
Sébastien Kurtzemann <seb@kurtzemann.fr>
2020-04-23 00:01:34 +0200

    feat: improve gbt prompt config

--- config/home/oh-my-zsh/custom/gbt.zsh
+++ config/home/oh-my-zsh/custom/gbt.zsh
@@ -1,8 +1,10 @@
-export GBT_CARS='Status, Os, Hostname, Dir, Kubectl, Git, Sign'
+export GBT_CARS='Status, Hostname, Dir, Kubectl, Git, Sign'
+export GBT_CAR_STATUS_FORMAT=' {{ Symbol }} {{ Signal }} '
 export GBT_CAR_HOSTNAME_FORMAT=' {{ User }} '
-export GBT_CAR_DIR_DEPTH='2'
+export GBT_CAR_DIR_DEPTH='10'
 export GBT_CAR_DIR_BG="yellow"
 export GBT_CAR_DIR_FG="black"
 export GBT_CAR_KUBECTL_BG="blue"
 export GBT_CAR_KUBECTL_FORMAT=' {{ Icon }} {{Context}}:{{Namespace}} '
 export GBT_CAR_KUBECTL_NAMESPACE_FM="bold"
+export GBT_CAR_SIGN_WRAP="1"
