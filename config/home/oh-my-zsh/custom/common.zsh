5236ab99b32a7c645e10a13ba9e629dc29d3f395
Seb <seb@kurtzemann.fr>
2020-07-20 15:28:54 +0200

    chore: remove pbcopy and pbpaste aliases

--- config/home/oh-my-zsh/custom/common.zsh
+++ config/home/oh-my-zsh/custom/common.zsh
@@ -9,10 +9,3 @@ alias u="source ~/.zshrc"
 alias v="vagrant"
 
 alias ll="ls -al"
-
-alias hosts="sudo /home/linuxbrew/.linuxbrew/bin/hosts"
-
-alias xclip="xclip -selection c"
-
-alias pbcopy='xclip -selection clipboard'
-alias pbpaste='xclip -selection clipboard -o'
