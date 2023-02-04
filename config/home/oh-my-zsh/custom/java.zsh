cee1cab13d016beb3217036ade42f7da8d788d38
Sébastien Kurtzemann <seb@kurtzemann.fr>
2023-02-04 21:17:42 +0100

    feat(zsh): java binaries are managed by asdf
    From macstrap

--- config/home/oh-my-zsh/custom/java.zsh
+++ config/home/oh-my-zsh/custom/java.zsh
@@ -1,2 +1,12 @@
-export JAVA_HOME=$(/usr/libexec/java_home)
-# export PATH=$PATH:$JAVA_HOME/bin
+# Add ASDF to ZSH
+. $(brew --prefix asdf)/libexec/asdf.sh
+
+# Set JAVA_HOME
+. ~/.asdf/plugins/java/set-java-home.zsh
+
+# Export JDK environment variable used for build
+export JDK8=$(asdf where java liberica-8u342+7)
+export JDK11=$(asdf where java liberica-11.0.16+8)
+export JDK16=$(asdf where java liberica-16.0.2+7)
+export JDK17=$(asdf where java liberica-17.0.4+8)
+export JDK18=$(asdf where java liberica-18.0.2+10)
