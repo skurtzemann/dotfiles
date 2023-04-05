59ef6f9be5c28d7c0d71d37fb867eba950a57a28
Sébastien Kurtzemann <seb@kurtzemann.fr>
2023-04-05 14:40:42 +0200

    feat(zsh): update if block indentation in aws-ssh

--- config/home/oh-my-zsh/custom/aws.zsh
+++ config/home/oh-my-zsh/custom/aws.zsh
@@ -18,22 +18,20 @@ One connected you can run one of following commands:
         local awsInstanceFile=$(find ${AWS_CONFIG_DIR} -type f -execdir basename '{}' ';' | fzf)
 
         if [[ "${awsInstanceFile}" != "" ]]; then
+            local instance=$(yq -r '.instance.id' ${AWS_CONFIG_DIR}/${awsInstanceFile})
+            local profile=$(yq -r '.instance.profile' ${AWS_CONFIG_DIR}/${awsInstanceFile})
+            local region=$(yq -r '.instance.region' ${AWS_CONFIG_DIR}/${awsInstanceFile})
 
-        local instance=$(yq -r '.instance.id' ${AWS_CONFIG_DIR}/${awsInstanceFile})
-        local profile=$(yq -r '.instance.profile' ${AWS_CONFIG_DIR}/${awsInstanceFile})
-        local region=$(yq -r '.instance.region' ${AWS_CONFIG_DIR}/${awsInstanceFile})
+            echo -e "Instance: ${GREEN}${instance}${ENDCOLOR}"
+            echo -e "Profile : ${GREEN}${profile}${ENDCOLOR}"
+            echo -e "Region  : ${GREEN}${region}${ENDCOLOR}"
 
-        echo -e "Instance: ${GREEN}${instance}${ENDCOLOR}"
-        echo -e "Profile : ${GREEN}${profile}${ENDCOLOR}"
-        echo -e "Region  : ${GREEN}${region}${ENDCOLOR}"
+            local target=$(aws --profile "${profile}" --region "${region}" ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:Name,Values=${instance}" --query "Reservations[0].Instances[0].InstanceId" --output text)
 
-        local target=$(aws --profile "${profile}" --region "${region}" ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:Name,Values=${instance}" --query "Reservations[0].Instances[0].InstanceId" --output text)
+            echo -e "Target  : ${RED}${target}${ENDCOLOR}"
 
-        echo -e "Target  : ${RED}${target}${ENDCOLOR}"
-
-        echo -e $connectCommands
-        aws ssm start-session --profile "${profile}" --target ${target} --region "${region}"
-
+            echo -e $connectCommands
+            aws ssm start-session --profile "${profile}" --target ${target} --region "${region}"
         fi
     fi
 }
