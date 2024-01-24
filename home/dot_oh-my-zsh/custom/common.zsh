d231624ed247e266ca075c3b9935d59f065763e3
Kurtzemann Sébastien <seb@kurtzemann.fr>
2024-01-24 22:16:00 +0100

    feat(zsh): add common.zsh custom script

---
+++ home/dot_oh-my-zsh/custom/common.zsh
@@ -0,0 +1,18 @@
+alias b="brew"
+alias bb="brew bundle"
+
+alias cdsp="cd $HOME/Private/Sources/"
+
+alias s="sudo"
+alias t="task"
+alias u="source ~/.zshrc"
+alias v="vagrant"
+
+alias ll="ls -al"
+
+alias python="python3"
+alias vmshow="ps -Af | grep hyperkit"
+
+function diff {
+  colordiff -u "$@"
+}
