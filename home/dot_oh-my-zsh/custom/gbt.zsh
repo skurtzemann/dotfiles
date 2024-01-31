1caaad7ea1f4759a370f719c5b7e45cae8ab545f
Kurtzemann Sébastien <seb@kurtzemann.fr>
2024-01-31 21:50:29 +0100

    feat(zsh): enable and configure gbt

---
+++ home/dot_oh-my-zsh/custom/gbt.zsh
@@ -0,0 +1,16 @@
+export GBT_CARS='Status, Dir, Kubectl, Git, Sign'
+export GBT_CAR_STATUS_FORMAT=' {{ Symbol }} {{ Signal }} '
+export GBT_CAR_HOSTNAME_FORMAT=' {{ User }} '
+export GBT_CAR_DIR_DEPTH='5'
+export GBT_CAR_DIR_BG="yellow"
+export GBT_CAR_DIR_FG="black"
+export GBT_CAR_KUBECTL_BG="blue"
+export GBT_CAR_KUBECTL_FORMAT=' {{ Icon }} {{Context}}:{{Namespace}} '
+export GBT_CAR_KUBECTL_NAMESPACE_FM="bold"
+export GBT_CAR_GIT_FORMAT=' {{ Icon }} {{ Head }} {{ Status }}{{ Ahead }}{{ Behind }}{{ Stash }} '
+export GBT_CAR_GIT_AHEAD_FORMAT='{{ AheadSymbol }}(+{{ AheadCount }})'
+export GBT_CAR_GIT_BEHIND_FORMAT='{{ BehindSymbol }}(-{{ BehindCount }})'
+export GBT_CAR_GIT_STASH_FORMAT='{{ StashSymbol }}({{ StashCount }})'
+export GBT_CAR_SIGN_USER_TEXT='🔍 >'
+export GBT_CAR_SIGN_USER_FG='blue'
+export GBT_CAR_SIGN_WRAP="1"
