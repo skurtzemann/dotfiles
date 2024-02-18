f124a755a2c92dc30372d48b46d76683380ac628
Sébastien Kurtzemann <seb@kurtzemann.fr>
2024-02-18 20:53:49 +0100

    feat(zsh): add aliases for ghq

--- home/dot_oh-my-zsh/custom/common.zsh
+++ home/dot_oh-my-zsh/custom/common.zsh
@@ -2,6 +2,7 @@ alias b="brew"
 alias bb="brew bundle"
 
 alias cdsp="cd $HOME/Private/Sources/"
+alias ghqsp="ghq list | grep -v algolia"
 
 alias s="sudo"
 alias t="task"
--- home/dot_oh-my-zsh/custom/work.zsh
+++ home/dot_oh-my-zsh/custom/work.zsh
@@ -1,1 +1,2 @@
 alias cds="cd $HOME/Work/Sources/"
+alias ghqs="ghq list | grep algolia"
